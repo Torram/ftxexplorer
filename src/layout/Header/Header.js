c30234fa5e0d9410c17544525d0cac29594c0c52
Raymundo Torres Ramírez <raymundotorres@MacBook-de-Raymundo.local>
2022-09-12 01:28:13 -0500

    templates

---
+++ src/layout/Header/Header.js
@@ -0,0 +1,16 @@
+/*
+Component: src/component/Header/Header.js
+*/
+
+import React from 'react';
+import './Header.scss';
+import {RouteComponentProps} from 'react-router-dom';
+
+const Header = (props) => {
+    return (
+        <div className = "Header">
+            {props.name} Component
+        </div>
+    )
+}
+export default Header; 
---
+++ templates/page/component.js
@@ -0,0 +1,16 @@
+/*
+Component: src/pages/TemplateName/TemplateName.js
+*/
+
+import React from 'react';
+import './TemplateName.scss';
+import {RouteComponentProps} from 'react-router-dom';
+
+const TemplateName = (props) => {
+    return (
+        <div className = "TemplateName">
+            {props.name} Component
+        </div>
+    )
+}
+export default TemplateName; 
